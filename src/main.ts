df67eccde75679bd57dc4d251159e4b0fc71428c
Rene Olivo <minet.ws@gmail.com>
2019-02-11 00:21:26 +0000

    Add base players list layout

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import 'bootstrap/scss/bootstrap.scss';
-import 'font-awesome/scss/font-awesome.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
